2821925abd56275d472868ecca315c7b2cc49ae6
learndev77 <restrella@77global.biz>
2024-06-18 14:16:30 +0800

    'expressjs'

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const http = require('http')
-const routes = require('./routes')
+const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const path = require("path");
 
-console.log(routes.someText)
-const server = http.createServer(routes.handler)
+const app = express();
+// app.use((req, res, next) => {
+//   console.log("always run this code");
+//   next();
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-server.listen(3000)
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+// const server = http.createServer(app);
+// server.listen(3000)
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
 //
--- routes.js
+++ routes.js
@@ -26,15 +26,16 @@ const requestHandler = (req, res) => {
         })
 
         return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            console.log(parsedBody)
-            const message = parsedBody.split('=')[0]
-            
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            })
+          const parsedBody = Buffer.concat(body).toString();
+          console.log(parsedBody);
+
+          const message = parsedBody.split("=")[0];
+
+          fs.writeFile("message.txt", message, (err) => {
+            res.statusCode = 302;
+            res.setHeader("Location", "/");
+            return res.end();
+          });
         })
         
     }
